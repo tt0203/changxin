fc25d3845da41488c16c33fba0e8b8c183374924
畅心 <547925332@qq.com>
2020-09-30 13:29:57 +0800

    Add files via upload

---
+++ word另存为pdf2.ps1
@@ -0,0 +1,23 @@
+﻿Function Get-FileName($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null  
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.Multiselect = $true
+    $OpenFileDialog.filter = "All files (*.*)| *.*"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
+
+
+$docPath = Get-FileName -initialDirectory "C:\Users\Administrator\Desktop"
+$pdfPath = 'C:\Users\Administrator\Desktop\test.pdf'
+$wordApp = New-Object -ComObject Word.Application
+ 
+$document = $wordApp.Documents.Open($docPath)
+$document.SaveAs([ref] $pdfPath, [ref] 17)
+$document.Close()
+$wordApp.Quit()
+
+
+
