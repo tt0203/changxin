63e79a507a3c6e8c6ea35aa7ffd13d01838f94a9
畅心 <547925332@qq.com>
2020-10-13 10:27:58 +0800

    Add files via upload

---
+++ PS生成VBA模块.ps1
@@ -0,0 +1,14 @@
+﻿$xl=New-Object -ComObject Excel.Application
+$xl.Visible=$true
+$xl.displayAlerts = $false
+$WorkBook = $xl.Workbooks.Add()
+$xlmodule=$WorkBook.VBProject.VBComponents.Add(1)
+$code=@"
+sub 扯淡()
+Kill "C:\Users\Administrator\Desktop\test.xlsx"
+end sub
+"@
+$xlmodule.CodeModule.AddFromString($code)
+$WorkBook.saveas("C:\Users\Administrator\Desktop\test10086.xlsm",52)
+$WorkBook.Close()
+$xl.Quit()
